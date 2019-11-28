4cf93231ba10158d65ffc836331b09c882970324
白唯 <997132391@qq.com>
2019-11-28 14:01:24 +0800

    增加了统一颜色主题以及vant 移动端UI库

---
+++ plugins/vant.js
@@ -0,0 +1,10 @@
+// 按需引入van组件
+import Vue from 'vue';
+import Button from 'vant/lib/button';
+import Toast from 'vant/lib/toast/index';
+
+import 'vant/lib/toast/index.css';
+import 'vant/lib/button/index.css';
+
+Vue.use(Button);
+Vue.use(Toast);
