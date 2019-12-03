6a8397f5a4f42cae8662c043ebbc5b34132a81d1
1368590868 <1368590868@qq.com>
2019-12-04 00:27:34 +0800

    '新增地图配置文件'

---
+++ plugins/vue-amap.js
@@ -0,0 +1,11 @@
+import AMap from 'vue-amap';
+import Vue from 'vue';
+Vue.use(AMap);
+
+  // 初始化vue-amap
+AMap.initAMapApiLoader({
+  // 高德key
+  key: '2a288da476b9ca0f66c0d0e64ddc28f7',
+  // 插件集合 （插件按需引入）
+  plugin: ['AMap.Geolocation']
+});
