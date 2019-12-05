a1da2cc6c559af8bda8256c1838a634f278cfd1e
q1071438836 <1071438836@qq.com>
2019-12-05 23:59:33 +0800

    学生作品优化，添加about页面

---
+++ pages/about/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app-container">
+    <!-- 画室环境 -->
+    <paintingEnvironment v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
+
+    <!-- 师资力量 -->
+    <teachers v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
+
+    <!-- 学生作品 -->
+    <worksCarous v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
+
+    <!-- 优势 -->
+    <advantage v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
+
+    <!-- 底部菜单栏 -->
+    <message-board v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
+  </div>
+</template>
+
+<script>
+import advantage from '../../components/works/advantage';
+import teachers from '../../components/works/teachers';
+import worksCarous from '../../components/index/worksCarousel';
+import MessageBoard from '../../components/common/messageBoard';
+import paintingEnvironment from '../../components/index/paintingEnvironment';
+import { Color } from '../../config/color';
+export default {
+  components: {
+    advantage,
+    worksCarous,
+    MessageBoard,
+    paintingEnvironment,
+    teachers
+  },
+  data() {
+    return {
+      Color
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped></style>
