cd2d6236071f194a70e92f41c82c9d9956f33ad2
ranlin520 <1368590868@qq.com>
2019-12-06 17:06:43 +0800

    'PC端优化'

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,32 +2,18 @@
   <div class="contact">
     <!-- 头部图片信息 -->
     <div class="contact-img" v-scroll-reveal.scaleUp="{scale:0.15}">
-      <van-image class="bgImg" width="100%" :src="bgUrl" />
       <div class="bg-title">
         <h1>您好，很高兴为您服务</h1>
-        <h2>
-          2019年创办于重庆大学城，两兄弟所办,朝气蓬勃,全年开课，随到随学，自由选择学习时间,给我们留下您宝贵的留言吧
-        </h2>
+        <h2>2019年创办于重庆大学城，两兄弟所办,朝气蓬勃,全年开课，随到随学，自由选择学习时间,给我们留下您宝贵的留言吧</h2>
       </div>
     </div>
 
     <div class="online" v-scroll-reveal.smooth="{easing:'ease-in'}">
-      <v-title :init-title='initTtile'></v-title>
+      <v-title :init-title="initTtile"></v-title>
       <!-- 表单内容 -->
       <van-cell-group class="form">
-        <van-field
-          v-model="name"
-          label="姓 名"
-          placeholder="您的姓名"
-          label-class="name"
-          required
-        />
-        <van-field
-          v-model="phone"
-          label="电 话"
-          placeholder="您的电话号码"
-          required
-        />
+        <van-field v-model="name" label="姓 名" placeholder="您的姓名" label-class="name" required />
+        <van-field v-model="phone" label="电 话" placeholder="您的电话号码" required />
         <van-field v-model="weChat" label="微 信" placeholder="您的的微信号" />
         <van-field
           v-model="message"
@@ -40,11 +26,9 @@
           show-word-limit
         />
       </van-cell-group>
-      <van-button class="submit" size="large" @click="submit"
-        >提交信息</van-button
-      >
+      <van-button class="submit" size="large" @click="submit">提交信息</van-button>
     </div>
-    <v-title :init-title='initMap' v-scroll-reveal.smooth="{easing:'ease-in'}"></v-title>
+    <v-title :init-title="initMap" v-scroll-reveal.smooth="{easing:'ease-in'}"></v-title>
     <!-- 插入地图 -->
     <g-map></g-map>
   </div>
@@ -54,13 +38,12 @@
 import vTitle from '../../components/common/vTitle'
 import gMap from '../../components/common/gMap'
 export default {
-  components:{
+  components: {
     vTitle,
     gMap
   },
-  data() {
+  data () {
     return {
-      bgUrl: require('../../assets/images/index/worksImg04.png'),
       // 表单绑定值
       name: '',
       phone: '',
@@ -75,7 +58,7 @@ export default {
         icon: '&#xe66f;'
       },
       //地图标题
-      initMap:{
+      initMap: {
         cnTitle: '联系地址',
         enTitle: 'Contact Address',
         mode: 'black',
@@ -88,7 +71,7 @@ export default {
      * submit 提交所有数据
      * @return void
      */
-    submit() {
+    submit () {
       this.$toast('提交了所有数据');
     }
   }
@@ -99,8 +82,11 @@ export default {
 .contact {
   .contact-img {
     position: relative;
+    background-image: url(../../assets/images/index/worksImg04.png);
+    width: 100%;
+    height: 400px;
     .bg-title {
-        padding: 20px;
+      padding: 20px;
       position: absolute;
       display: flex;
       top: 0;
@@ -111,12 +97,12 @@ export default {
       width: 100%;
       height: 100%;
       h1 {
-        font-size: 24px;
+        font-size: 18px;
         font-weight: bold;
         color: #fff;
       }
       h2 {
-        font-size: 16px;
+        font-size: 14px;
         text-align: center;
         color: #fff;
         margin-top: 15px;
@@ -133,21 +119,24 @@ export default {
       width: 100%;
       margin-top: 20px;
     }
-    .submit{
-        margin-bottom: 30px;
+    .submit {
+      margin-bottom: 30px;
     }
     .online-desc {
       font-size: 18px;
       font-weight: bold;
     }
   }
-  .more-links{
+  .more-links {
     display: flex;
     flex-flow: row wrap;
     justify-content: center;
     align-items: center;
     margin-bottom: 20px;
-    .qq,.Email,.tell,.weChat{
+    .qq,
+    .Email,
+    .tell,
+    .weChat {
       width: 50%;
       margin-top: 10px;
       display: flex;
@@ -158,28 +147,29 @@ export default {
   }
 }
 @media screen and (min-width: 720px) {
-  .contact{
+  .contact {
     width: 100%;
-    .contact-img{
-      .bg-title{
+    .contact-img {
+      height:400px;
+      background-position: center;
+      .bg-title {
         padding: 20px;
-        h1{
-          font-size: 3vw;
+        h1 {
+          font-size: 32px;
         }
-        h2{
-          font-size: 2vw;
+        h2 {
+          font-size: 20px;
         }
       }
     }
-    .online{
-      .form{
-  max-width: 450px;
-      margin: 0 auto;
+    .online {
+      .form {
+        max-width: 450px;
+        margin: 0 auto;
       }
-      .submit{
+      .submit {
         max-width: 450px;
       }
-    
     }
   }
 }
