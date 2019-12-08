191c5b80a64b107d231dadf3cc9ba5382c92423f
q1071438836 <1071438836@qq.com>
2019-12-08 23:45:34 +0800

    详情页教师资料list

---
+++ components/about/teacher.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="teacher">
+    <v-title :init-title="initTtile"></v-title>
+    <card :card-list="teacherList" />
+  </div>
+</template>
+
+<script>
+import card from '@/components/common/card'
+import vTitle from '@/components/common/vTitle';
+export default {
+  components: {
+    vTitle,
+    card
+  },
+  data () {
+    return {
+      initTtile: {
+        cnTitle: '教师资源',
+        enTitle: 'Teacher Resources',
+        mode: 'block',
+        icon: '&#xe618;'
+      },
+      teacherList: [
+        {
+          id: 1,
+          name: '田奇',
+          desc: '专业水墨画老师，多次获得国赛一等奖',
+          headImg: require('../../assets/images/index/cat.png'),
+          rate: 4.5
+        },
+        {
+          id: 2,
+          name: '田奇2',
+          desc: '北京艺术大学毕业，参与过多次xxx奖的评选',
+          headImg: require('../../assets/images/index/cat.png'),
+          rate: 5
+        },
+        {
+          id: 3,
+          name: '田奇3',
+          desc: '热情好学，乐于帮助同学解决问题',
+          headImg: require('../../assets/images/index/cat.png'),
+          rate: 4.7
+        },
+        {
+          id: 4,
+          name: '田奇4',
+          desc: '坦克驾驶员',
+          headImg: require('../../assets/images/index/cat.png'),
+          rate: 4.0
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='less' scope>
+@import url('../../assets/css/color.less');
+.teacher {
+  background: @color-grey-5;
+}
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,41 +1,19 @@
 <template>
-  <div class="app-container">
-    <!-- 画室环境 -->
-    <paintingEnvironment v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
-
-    <!-- 师资力量 -->
-    <teachers v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
-
-    <!-- 学生作品 -->
-    <worksCarous v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
-
-    <!-- 优势 -->
-    <advantage v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
-
-    <!-- 底部菜单栏 -->
-    <message-board v-scroll-reveal.smooth="{ easing: 'ease-in' }" />
+  <div>
+    <paintEnvironment />
+    <teacher />
   </div>
 </template>
 
 <script>
-import advantage from '../../components/works/advantage';
-import teachers from '../../components/works/teachers';
-import worksCarous from '../../components/index/worksCarousel';
-import MessageBoard from '../../components/common/messageBoard';
-import paintingEnvironment from '../../components/index/paintingEnvironment';
-import { Color } from '../../config/color';
+import paintEnvironment from '@/components/about/paintEnvironment';
+import teacher from '@/components/about/teacher';
 export default {
   components: {
-    advantage,
-    worksCarous,
-    MessageBoard,
-    paintingEnvironment,
-    teachers
+    paintEnvironment,
+    teacher
   },
-  data() {
-    return {
-      Color
-    };
+  data () {
   },
   methods: {}
 };
