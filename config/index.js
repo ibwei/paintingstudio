2c0a6c1eafc16cd68bfe232d4eda5a60cde368d0
白唯 <997132391@qq.com>
2019-12-27 11:38:21 +0800

    fix：api

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 // 登录URL
 export const LOGIN_URL = 'https://www.baidu.com';
 
-export const API = '/api';
+export const API = 'http://www.paintingapi.ibwei.com/api';
 
 // 3D画廊的远程地址
 export const GalleryUrl = 'http://paintingworks.ibwei.com';
